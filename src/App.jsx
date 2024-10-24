36e2bf4ac4914b0c9fe6e9b2f618830f34788856
smallproject <j.a.villas@live.com.ph>
2024-10-24 15:25:44 +0200

    werkende applicatie

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { AuthContext } from './context/AuthContext';
 import './App.css';
 
 function App() {
+  const { isAuth } = useContext(AuthContext);
+
   return (
     <>
       <NavBar />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/profile" element={isAuth ? <Profile /> : <Navigate to="/" />} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signup" element={<SignUp/>} />
         </Routes>
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../assets/banana-01.png';
 import { useNavigate, Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function NavBar() {
+  const { isAuth, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
     <nav>
-        <Link to="/">
+      <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
             <h3>
               Banana Security
             </h3>
           </span>
-        </Link>
+      </Link>
 
-      <div>
+      {isAuth ?
         <button
           type="button"
-          onClick={() => navigate('/signin')}
+          onClick={logout}
         >
-          Log in
+          Log uit
         </button>
-        <button
-          type="button"
-          onClick={() => navigate('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
+        :
+        <div>
+          <button
+            type="button"
+            onClick={() => navigate('/signin')}
+          >
+            Log in
+          </button>
+          <button
+            type="button"
+            onClick={() => navigate('/signup')}
+          >
+            Registreren
+          </button>
+        </div>
+      }
     </nav>
   );
 }
