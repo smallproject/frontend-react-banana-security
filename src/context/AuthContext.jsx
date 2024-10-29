cb2dbe88364656d9cb5553ccb5692f0f319bb938
smallproject <j.a.villas@live.com.ph>
2024-10-29 11:10:27 +0100

    authentication added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,40 @@
 import React, { createContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {jwtDecode} from "jwt-decode";
 
 export const AuthContext = createContext({});
 
 function AuthContextProvider({ children }) {
   const [isAuth, toggleIsAuth] = useState(false);
   const navigate = useNavigate();
+  const [auth, setAuth] = useState({
+    isAuth: false,
+    user : {
+      username: '',
+      email: '',
+      password: ''
+    }
+  });
 
-  function login() {
+  async function login(token) {
+    localStorage.setItem('token', token)
+
+    const decodedToken = jwtDecode(token);
+    console.log(decodedToken.sub);
+
+    try {
+      const response = await axios.get(`https://localhost:3000/600/users/${decodedToken}`);
+
+      setAuth({
+        isAuth: true,
+        user: {
+          username: 'Test',
+          email: ''
+        }});
+    } catch (e) {
+      console.log(e)
+    }
+
     console.log('Gebruiker is ingelogd!');
     toggleIsAuth(true);
     navigate('/profile');
